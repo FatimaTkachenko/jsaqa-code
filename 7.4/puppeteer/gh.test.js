0d85d3fd928f67fc1eec1f58a8cb0dfcde817d27
Fatima <fat.tka4enko2014@yandex.ru>
2026-05-25 19:36:40 +0300

    Добавлены 3 теста за блоком describe для других страниц приложения, хуки перенесены внутрь describe

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,6 +1,6 @@
 let page;
 
-describe("Github Team page tests", () => {
+describe("Github page tests", () => {
   beforeEach(async () => {
     page = await browser.newPage();
     await page.goto("https://github.com/team");
@@ -31,50 +31,32 @@ describe("Github Team page tests", () => {
   });
 });
 
-describe("Github About page tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://github.com/about");
-  });
-
-  afterEach(() => {
-    page.close();
-  });
-
-  test("The h1 header content on About page", async () => {
-    await page.waitForSelector('h1');
-    const title = await page.title();
-    expect(title).toEqual('About GitHub · GitHub');
-  });
-
-  test("The page contains h1 with search text", async () => {
-    const h1Text = await page.$eval('h1', el => el.textContent);
-    expect(h1Text).toContain("Search code, repositories, users, issues, pull requests");
-  });
+// Новые тесты за блоком describe — для других страниц приложения
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto("https://github.com/about");
 });
 
-describe("Github Pricing page tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://github.com/pricing");
-  });
+afterEach(() => {
+  page.close();
+});
 
-  afterEach(() => {
-    page.close();
-  });
+test("The h1 header content on About page", async () => {
+  await page.waitForSelector('h1');
+  const title = await page.title();
+  expect(title).toEqual('About GitHub · GitHub');
+});
 
-  test("The h1 header content on Pricing page", async () => {
-    await page.waitForSelector('h1');
-    const title = await page.title();
-    expect(title).toEqual('Pricing · Plans for every developer · GitHub');
-  });
+test("The page contains h1 with search text", async () => {
+  const h1Text = await page.$eval('h1', el => el.textContent);
+  expect(h1Text).toContain("Search code, repositories, users, issues, pull requests");
+});
 
-  test("The page contains Start free button", async () => {
-    const btnSelector = ".btn-mktg";
-    await page.waitForSelector(btnSelector, {
-      visible: true,
-    });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Start free")
+test("The page contains Start free button", async () => {
+  const btnSelector = ".btn-mktg";
+  await page.waitForSelector(btnSelector, {
+    visible: true,
   });
+  const actual = await page.$eval(btnSelector, link => link.textContent);
+  expect(actual).toContain("Subscribe");
 });
